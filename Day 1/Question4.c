5fe8a46321a19e0b3a60d5736bfb0bb39d81e97b
amanbhardwajbaro <amanbhardwajbaro@gmail.com>
2026-05-29 19:00:57 +0530

    Day 1 questions

---
+++ Day 1/Question4.c
@@ -0,0 +1,16 @@
+//count digits of a given number 
+#include<stdio.h>
+int main(){
+    int n,count=0;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    if(n==0)
+    {count==1;}
+    else
+    {while(n!=0)
+      {n=n/10;
+      count++; }
+    }
+    printf("The digits of the given number : %d",count);
+    return 0;
+}
