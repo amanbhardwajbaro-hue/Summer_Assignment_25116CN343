17b7f8b57f096800f7d85f1122c6cf814e699150
amanbhardwajbaro <amanbhardwajbaro@gmail.com>
2026-06-08 17:38:56 +0530

    Day 9

---
+++ Day 9/Question4.c
@@ -0,0 +1,13 @@
+//Hollow square pattern
+#include <stdio.h>
+int main() {
+    int i,j;
+    for(i=1;i<=5;i++)
+    {for(j=1;j<=5;j++)
+    {if((i==2 || i==3 || i==4)&&(j==2 || j==3 || j==4))
+    {printf(" ");}
+else
+{printf("*");}}
+printf("\n");}
+return 0;
+    }
